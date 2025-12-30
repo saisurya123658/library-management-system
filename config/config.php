243ac8d74463eb1e5bb7ab4a5e29356e69eb28e5
AREVETI SAI GURU SURYA TEJA <154744703+saisurya123658@users.noreply.github.com>
2025-12-30 17:19:47 +0530

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+// Base URL (adjust if your folder is inside htdocs or a subfolder)
+$BASE_URL = "/Library_Managment/";
+
+// Upload directories
+$BOOK_COVER_PATH = __DIR__ . "/../uploads/book_covers/";
+$AUDIO_PATH = __DIR__ . "/../uploads/audio/";
+$DOCS_PATH = __DIR__ . "/../uploads/documents/";
+
+// Return password for admin (change manually later)
+$ADMIN_RETURN_PASSWORD = "admin123";
+?>
+
